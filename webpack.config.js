494806d36a7fd3b6ad6cb5f768de2df076d8e307
Kyle Uehlein <kyleuehlein@gmail.com>
2017-12-19 17:16:09 -0500

    feat: add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const LiveReloadPlugin = require('webpack-livereload-plugin')
+const isDev = process.env.NODE_ENV === 'development'
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: isDev ? [new LiveReloadPlugin({ appendScriptTag: true })] : []
+}
